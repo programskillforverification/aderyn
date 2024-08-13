09acfc6901236477700bd2ac89e44d93b3e93138
Joseph Zhao <zhaozihan542259545@gmail.com>
2024-08-13 22:18:12 +0800

    test new ci

---
+++ aderyn_py/tests/test_generate_report.py
@@ -0,0 +1,14 @@
+from unittest.mock import Mock
+from aderynpy import generate_report
+
+# generate_report("../../tests/contract-playground", "./report.md")
+
+# generate_report()
+
+# generate_report("../tests/contract-playground", "./report.md", 
+#     path_includes=["src/eth2", "src/inheritance"],
+# )
+
+# generate_report("../tests/contract-playground", "./report.md", 
+#     path_includes=None,
+# )
